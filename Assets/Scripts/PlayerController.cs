ca476fb95fb4c7724ae4ec94809b57c1b05e51c2
Bryan <bryancrim@gmail.com>
2020-01-27 16:31:50 +0100

    Fix PlayerController Scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerController : MonoBehaviour
 {
+
+    public KeyCode pressLeft;
+    public KeyCode pressRight;
+    
     private Rigidbody2D body;
     private Vector2 direction;
 
-    private float speed = 1f;
-    private float movement = 0f;
+    private float speed = 3f;
+    private float movementY = 0f;
+    private float movementX = 0f;
     
     // Start is called before the first frame update
     void Start()
@@ -20,7 +24,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(movement * speed, body.velocity.y);
+        movementY = Input.GetAxis("Vertical");
+        movementX = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(movementX * speed, movementY * speed);
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            GetComponent<Transform>().eulerAngles += new Vector3(0, 0, 2f);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            GetComponent<Transform>().eulerAngles += new Vector3(0, 0, -2f);
+        }
     }
 }
