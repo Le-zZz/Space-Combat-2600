8c25dc5befffece8b6216fd83a2261794e868941
Bryan <bryancrim@gmail.com>
2020-01-27 19:49:56 +0100

    ClickToMove Scripts, Player2Controller added

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour
+{
+
+    [SerializeField] float speed = 1;
+    private Vector3 targetPosition;
+    bool isMoving = false;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            SetTargetPosition();
+        }
+
+        if (isMoving)
+        {
+            Move();
+        }
+    }
+
+    void SetTargetPosition()
+    {
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition.z = transform.position.z;
+
+        isMoving = true;
+    }
+
+    void Move()
+    {
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPosition);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        if (transform.position == targetPosition)
+        {
+            isMoving = false;
+        }
+    }
+}
